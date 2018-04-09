529908b978bee5ef658276515086d1f7fb7c8d5d
huipeng <huipengly@gmail.com>
2018-04-09 14:49:28 +0800

    工程还有引用没配好，直接复制完整的

---
+++ Samples/LightBuzz.Vitruvius.Samples.WPF/AnglePage.xaml.cs
@@ -0,0 +1,145 @@
+﻿using LightBuzz.Vitruvius;
+using Microsoft.Kinect;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace LightBuzz.Vituvius.Samples.WPF
+{
+    /// <summary>
+    /// Interaction logic for AnglePage.xaml
+    /// </summary>
+    public partial class AnglePage : Page
+    {
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        PlayersController _playersController;
+
+        JointType _start1 = JointType.ShoulderRight;
+        JointType _center1 = JointType.ElbowRight;
+        JointType _end1 = JointType.WristRight;
+
+        JointType _start2 = JointType.ElbowLeft;
+        JointType _center2 = JointType.ShoulderLeft;
+        JointType _end2 = JointType.SpineShoulder;
+
+        JointType _start3 = JointType.AnkleRight;
+        JointType _center3 = JointType.KneeRight;
+        JointType _end3 = JointType.HipRight;
+
+        public AnglePage()
+        {
+            InitializeComponent();
+
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+                _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+
+                _playersController = new PlayersController();
+                _playersController.BodyEntered += UserReporter_BodyEntered;
+                _playersController.BodyLeft += UserReporter_BodyLeft;
+                _playersController.Start();
+            }
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (_playersController != null)
+            {
+                _playersController.Stop();
+            }
+
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack();
+            }
+        }
+
+        void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            // Color
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (viewer.Visualization == Visualization.Color)
+                    {
+                        viewer.Image = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Body
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    var bodies = frame.Bodies();
+
+                    _playersController.Update(bodies);
+
+                    Body body = bodies.Closest();
+
+                    if (body != null)
+                    {
+                        viewer.DrawBody(body);
+
+                        Joint spine = body.Joints[JointType.SpineShoulder];
+                        Joint shoulder = body.Joints[JointType.ShoulderRight];
+                        Joint elbow = body.Joints[JointType.ElbowRight];
+                        Joint wrist = body.Joints[JointType.WristRight];
+                        Joint hand = body.Joints[JointType.HandRight];
+
+                        angle1.Update(shoulder, elbow, wrist, 50);
+                        angle2.Update(spine, shoulder, elbow, 50);
+                        angle3.Update(elbow, wrist, hand, 50);
+
+                        double angleWrist = wrist.Angle(elbow, hand, Axis.Y);
+                        double angleElbow = elbow.Angle(shoulder, wrist, Axis.Y);
+                        double angleShoulder = shoulder.Angle(spine, elbow, Axis.Y);
+
+                        tblAngle1.Text = ((int)angleElbow).ToString();
+                        tblAngle2.Text = ((int)angleShoulder).ToString();
+                        tblAngle3.Text = ((int)angleWrist).ToString();
+                    }
+                }
+            }
+        }
+
+        void UserReporter_BodyEntered(object sender, PlayersControllerEventArgs e)
+        {
+        }
+
+        void UserReporter_BodyLeft(object sender, PlayersControllerEventArgs e)
+        {
+            viewer.Clear();
+            angle1.Clear();
+            angle2.Clear();
+            angle3.Clear();
+
+            tblAngle1.Text = "-";
+            tblAngle2.Text = "-";
+            tblAngle3.Text = "-";
+        }
+    }
+}
